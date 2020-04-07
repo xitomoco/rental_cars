a6e87ac10ec72e65d69bc8e7bfebeb31599a12d2
Christian Moreira <chmleave@gmail.com>
2020-04-07 16:56:15 -0400

    method create manutacturers

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -7,4 +7,19 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:id])
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(params_manufacturer)
+    if @manufacturer.save
+      redirect_to manufacturer_url(@manufacturer)
+    end
+  end
+
+  def params_manufacturer
+    params.require(:manufacturer).permit(:name)
+  end
 end
